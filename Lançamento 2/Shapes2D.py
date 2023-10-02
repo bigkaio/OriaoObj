f31670e8c25c570900e9dc7da08c803d826a3da1
Kaio Macedo Santana <121952063+bigkaio@users.noreply.github.com>
2023-10-02 14:30:38 -0300

    Update Shapes2D.py

--- Lançamento 2/Shapes2D.py
+++ Lançamento 2/Shapes2D.py
@@ -1,1 +1,120 @@
 
+import math
+
+class Ponto():
+    def __init__(self, n, x, y):
+        self._numero = n
+        self._x = x
+        self._y = y
+
+    def distancia_entre_pontos(self, x, y):
+        return math.sqrt((self._x - x) * (self._x - x) + (self._y - y) * (self._y - y))
+
+    def getNumber(self):
+        return self._numero
+
+    def updateCoord(self, x, y):
+        self._x = x
+        self._y = y
+
+    def getType(self):
+        return "Point_"
+
+    def printCoord(self):
+        print(f'\nO ponto {self._numero} possui as coord: ({self._x}, {self._y}).')
+
+class Circulo(Ponto):
+    def __init__(self, n, x, y, raio):
+        super().__init__(n, x, y)
+        self._raio = raio
+
+    def printCoord(self):
+        print(f'\nO circulo {self._numero} possui as coord: ({self._x}, {self._y}) e seu raio é {self._raio}.')
+
+    def area(self):
+        return print(f'A area do circulo {self._numero} é {3.14 * (self._raio * self._raio)}.')
+
+    def getType(self):
+        return "Circle_"
+
+    def perimetro(self):
+        return print(f'O perimetro, ou circunferencia, do circulo {self._numero} é {2 * 3.14 * self._raio}.')
+
+    def updateCoord(self, x, y, raio):
+        super().updateCoord(x, y)
+        self._raio = raio
+
+    def pointIn(self, pt):
+        distancia = math.sqrt((self._x - pt._x) * (self._x - pt._x) + (self._y - pt._y) * (self._y - pt._y))
+        return print(f"O ponto {pt.getNumber()} se encontra dentro do circulo") if distancia <= self._raio else print(f"O ponto {pt.getNumber()} nao esta dentro do circulo")
+
+class Quadrado():
+    def __init__(self, n, lado):
+        self._lado = lado
+        self._numero = n
+
+    def area(self):
+        return print(f'A area do quadrado {self._numero} é {self._lado * self._lado}.')
+
+    def perimetro(self):
+        return print(f'O perimetro do quadrado {self._numero} é {self._lado * 4}.')
+
+    def diagonal(self):
+        return print(f'A diagonal do quadrado {self._numero} é {self._lado * 1.41}.')
+
+    def getType(self):
+        return "Quadrado_"
+
+    def getNumber(self):
+        return self._numero
+
+    def updateSize(self, lado):
+        self._lado = lado
+
+class Triangulo_equilatero(Quadrado):
+    def __init__(self, n, lado):
+        super().__init__(n, lado)
+
+    def area(self):
+        area = ((self._lado * self._lado) * 1.7) / 4
+        return print(f'A area do triangulo equilatero {self._numero} é {area}.')
+
+    def altura(self):
+        return print(f'A altura do triangulo equilatero {self._numero} é {(self._lado * 1.7) / 2}.')
+
+    def getNumber(self):
+        return self._numero
+
+    def perimetro(self):
+        return print(f'O perimetro do triangulo equilatero {self._numero} é {self._lado * 3}.')
+
+    def getType(self):
+        return 'Triangulo_'
+
+    def updateSize(self, lado):
+        super().updateSize(lado)
+
+class Retangulo(Quadrado):
+    def __init__(self, n, lado, altura):
+        super().__init__(n, lado)
+        self._lado = lado
+        self._altura = altura
+
+    def area(self):
+        return print(f'A area do retangulo {self._numero} é {self._lado * self._altura}.')
+
+    def perimetro(self):
+        return print(f'O perimetro do retangulo {self._numero} é {(2 * self._lado + 2 * self._altura)}.')
+
+    def diagonal(self):
+        return print(f'A diagonal do quadrado {self._numero} é {math.sqrt((self._lado * self._lado) + (self._altura * self._altura))}.')
+
+    def getType(self):
+        return 'Retangulo_'
+
+    def updateSize(self, lado, altura):
+        super().updateSize(lado)
+        self._altura = altura
+
+    def getNumber(self):
+        return self._numero
