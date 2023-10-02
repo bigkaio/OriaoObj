b0d66eab9586fbad3f17ec515c8ca018941135e2
Kaio Macedo Santana <121952063+bigkaio@users.noreply.github.com>
2023-10-02 14:30:55 -0300

    Update CoordSystem.py

--- Lançamento 2/CoordSystem.py
+++ Lançamento 2/CoordSystem.py
@@ -1,1 +1,30 @@
 
+import Shapes2D  
+
+class CartesianBoard:
+    def __init__(self):
+        self.shapes = {}
+
+    def insertShape(self, shape):
+        self.shapes[shape.getType() + str(shape.getNumber())] = shape
+
+    def removeShape(self, shape):
+        del self.shapes[shape.getType() + str(shape.getNumber())]
+
+    def showShapes(self):
+        print('\nEste plano cartesiano possui as seguintes formas:\n')
+        for shape in self.shapes.keys():
+            print(shape)
+
+    def printDetails(self):
+        for key in self.shapes.keys():
+            if isinstance(self.shapes[key], Shapes2D.Ponto) or isinstance(self.shapes[key], Shapes2D.Circulo):
+                self.shapes[key].printCoord()
+            if not isinstance(self.shapes[key], Shapes2D.Ponto):
+                self.shapes[key].area()
+                self.shapes[key].perimetro()
+                if not isinstance(self.shapes[key], Shapes2D.Circulo):
+                    self.shapes[key].diagonal()
+
+    def getShape(self, key):
+        return self.shapes[key]
