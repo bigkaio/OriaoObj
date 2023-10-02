f70e4bb43ba6f7547950f5e4f2953db67e1c5b29
Kaio Macedo Santana <121952063+bigkaio@users.noreply.github.com>
2023-10-02 14:38:24 -0300

    Update main.py

--- Lançamento 2/main.py
+++ Lançamento 2/main.py
@@ -1,1 +1,70 @@
+import Shapes2D
+import CoordSystem
 
+
+plano_cartesiano  = CoordSystem.CartesianBoard()
+
+ponto1 = Shapes2D.Ponto(1, 12, 25)
+plano_cartesiano.inserShape(ponto1)
+ponto1.printCoord()
+
+ponto2 = Shapes2D.Ponto(2, 7, 13)
+plano_cartesiano.inserShape(ponto2)
+ponto2.printCoord()
+
+print('\n')
+
+circulo3 = Shapes2D.Circulo(3, 8, 13, 5)
+plano_cartesiano.inserShape(circulo3)
+circulo3.printCoord()
+print(circulo3.getType())
+circulo3.area()
+circulo3.perimetro()
+circulo3.pointIn(ponto1)
+circulo3.pointIn(ponto2)
+
+print('\n')
+
+
+quadrado4 = Shapes2D.Quadrado(4, 5)
+plano_cartesiano.inserShape(quadrado4)
+quadrado4.area()
+quadrado4.perimetro()
+quadrado4.diagonal()
+print(quadrado4.getType())
+quadrado4.uptadeSize(6)
+quadrado4.area()
+
+print('\n')
+
+triangulo5 = Shapes2D.Triangulo_equilatero(5, 5)
+plano_cartesiano.inserShape(triangulo5)
+triangulo5.area()
+triangulo5.altura()
+print(triangulo5.getType())
+triangulo5.perimetro()
+triangulo5.uptadeSize(6)
+triangulo5.area()
+
+print('\n')
+
+retangulo6 = Shapes2D.Retangulo(6, 5, 4)
+plano_cartesiano.inserShape(retangulo6)
+retangulo6.area()
+retangulo6.perimetro()
+retangulo6.diagonal()
+print(retangulo6.getType())
+retangulo6.uptadeSize(3, 4)
+retangulo6.area()
+
+
+plano_cartesiano.showShapes()
+
+print('\n')
+
+plano_cartesiano.printDetails()
+
+
+plano_cartesiano.removeShape(ponto1)
+
+plano_cartesiano.showShapes()
